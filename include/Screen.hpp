a2cd2a1942352a31285ffc223aafb9151a3d6d08
Daniel Tyebkhan <dtyebkhan@gmail.com>
2024-12-15 17:16:54 -0500

    implement screen

--- include/Screen.hpp
+++ include/Screen.hpp
@@ -1,18 +1,36 @@
 #ifndef SCREEN_HPP
 #define SCREEN_HPP
 
+#include "Types.hpp"
 #include <array>
 #include <cstddef>
+#include <span>
 class Screen {
+  using Pixel = bool;
+
 public:
   void Clear();
-  void Draw();
 
-private:
-  bool _shouldDraw = true;
+  void Update();
+
+  /**
+   * @brief draw sprite to position `x` mod WIDTH, `y` mod HEIGHT. Sprites that
+   * exceed the screen's width or height are cut off at the edge (i.e. sprites
+   * do not wrap)
+   * @return true iff any pixels were erased, i.e. a collision occurred
+   */
+  bool Draw(Byte x, Byte y, const std::span<Byte> sprite);
+
+  void Display();
+
   constexpr static std::size_t WIDTH = 64;
   constexpr static std::size_t HEIGHT = 32;
-  std::array<bool, WIDTH * HEIGHT> _pixels;
+
+private:
+  static void ClearStdout();
+  Pixel &PixelAt(std::size_t x, std::size_t y);
+  bool _shouldRefresh = true;
+  std::array<Pixel, WIDTH * HEIGHT> _pixels = {};
 };
 
 #endif
---
+++ src/Screen.cpp
@@ -0,0 +1,65 @@
+#include "Screen.hpp"
+#include "Constants.hpp"
+#include "Types.hpp"
+#include <iostream>
+
+void Screen::Clear() {
+  for (auto &pixel : _pixels) {
+    pixel = false;
+  }
+}
+
+void Screen::ClearStdout() { std::cout << "\033[2J\033[1;1H"; }
+
+void Screen::Display() {
+  ClearStdout();
+  for (std::size_t y = 0; y < HEIGHT; ++y) {
+    for (std::size_t x = 0; x < WIDTH; ++x) {
+      std::cout << (PixelAt(x, y) ? '*' : ' ');
+    }
+  }
+  std::cout << std::flush;
+}
+
+void Screen::Update() {
+  if (_shouldRefresh) {
+    Display();
+    _shouldRefresh = false;
+  }
+}
+
+bool Screen::Draw(Byte x, Byte y, const std::span<Byte> sprite) {
+  constexpr static unsigned int MSB = 1 << (Constants::BITS_PER_BYTE - 1);
+  const auto xBase = x % WIDTH;
+  const auto yBase = y % HEIGHT;
+  _shouldRefresh = false;
+  bool collision = false;
+  for (std::size_t yOffset = 0; yOffset < sprite.size(); ++yOffset) {
+    const auto yCoord = yBase + yOffset;
+    if (yCoord > HEIGHT) {
+      break;
+    }
+    const auto spriteRow = sprite[yOffset];
+    for (unsigned int bitNum = 0; bitNum < Constants::BITS_PER_BYTE; ++bitNum) {
+      const auto xCoord = xBase + bitNum;
+      if (xCoord >= WIDTH) {
+        break;
+      }
+      auto &pixel = PixelAt(xCoord, y + yOffset);
+      const auto spriteBitSet = static_cast<bool>((MSB >> bitNum) & spriteRow);
+      const auto oldPixel = pixel;
+      pixel ^= static_cast<int>(spriteBitSet);
+      if (oldPixel != pixel) {
+        _shouldRefresh = true;
+        const bool pixelTurnedOff = !pixel;
+        collision = collision || pixelTurnedOff;
+      }
+    }
+  }
+  return collision;
+}
+
+Screen::Pixel &Screen::PixelAt(std::size_t x, std::size_t y) {
+  // NOLINTNEXTLINE
+  return _pixels[WIDTH * y + x];
+}
