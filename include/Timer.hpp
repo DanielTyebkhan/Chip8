7bb5659de4f1bfd56a813817af76f19f84365539
Daniel Tyebkhan <dtyebkhan@gmail.com>
2024-12-15 10:11:12 -0500

    separate timer start from construction

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -1,13 +1,18 @@
 #pragma once
 #include <chrono>
+#include <optional>
 class Timer {
 public:
   using Point = std::chrono::steady_clock::time_point;
   using Duration = std::chrono::steady_clock::duration;
-  Timer(Duration duration, Point now);
-  void Start(Duration duration, Point now);
+
+  explicit Timer(Duration duration);
+
+  void Start(Point now);
+
   bool IsDone(Point now);
 
 private:
-  std::chrono::steady_clock::time_point _finishTime;
+  Duration _duration;
+  std::optional<std::chrono::steady_clock::time_point> _finishTime;
 };
--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,11 +1,12 @@
 #include "Timer.hpp"
 
-Timer::Timer(Timer::Duration duration, Timer::Point now) {
-  Start(duration, now);
-}
+Timer::Timer(Timer::Duration duration) : _duration(duration) {}
 
-void Timer::Start(Timer::Duration duration, Timer::Point now) {
-  _finishTime = now + duration;
-}
+void Timer::Start(Timer::Point now) { _finishTime = now + _duration; }
 
-bool Timer::IsDone(Timer::Point now) { return now >= _finishTime; }
+bool Timer::IsDone(Timer::Point now) {
+  if (!_finishTime.has_value()) {
+    return false;
+  }
+  return now >= _finishTime;
+}
